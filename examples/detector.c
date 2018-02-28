be2bd6d5624b2624a6a5b2466e28d77fc0a9dbae
Alex Xu <xu_bin_han@hotmail.com>
2018-02-28 18:09:45 -0500

    Modified to go through sub-dir

--- examples/detector.c
+++ examples/detector.c
@@ -565,6 +565,109 @@ void validate_detector_recall(char *cfgfile, char *weightfile)
 	}
 }
 
+void _my_detect(char **names, network *net, char *filename, char *outfile, int fullscreen, float thresh, float hier_thresh, image **alphabet, int *count)
+{
+	int j;
+	double time;
+	float nms=.3;
+
+	char *input = malloc(sizeof(char) * 256);
+	// Use DIR to go through all files under the directory
+	DIR *d = opendir(filename);
+	if (!d) {
+		fprintf(stderr, "Failed to open directory\n");
+		return;
+	}
+	struct dirent *dir = readdir(d);
+	char *file;
+	while (dir) {
+		switch (dir -> d_type) {
+			case 4:
+				if (strcmp(dir -> d_name, ".") && strcmp(dir -> d_name, "..")) {
+					char * new_folder = malloc(sizeof(char) * 256);
+					strcpy(new_folder, filename);
+					strcat(new_folder, "/");
+					strcat(new_folder, dir->d_name);
+					_my_detect(names, net, new_folder, outfile, fullscreen, thresh, hier_thresh, alphabet, count);
+				}
+				break;
+			case 8:
+				file = dir -> d_name;
+				// check if the file ends with .jpg
+				if (strstr(file, ".jpg") != NULL) {
+
+					strcpy(input, filename);
+					strcat(input, "/\0");
+					strcat(input, file);
+
+					fprintf(stdout, "File curringly processing: %s\n", input);
+					(*count)++;
+					/*
+					   if(filename){
+					   strncpy(input, filename, 256);
+					   } else {
+					   printf("Enter Image Path: ");
+					   fflush(stdout);
+					   input = fgets(input, 256, stdin);
+					   if(!input) return;
+					   strtok(input, "\n");
+					   }
+					 */
+					image im = load_image_color(input,0,0);
+					image sized = letterbox_image(im, net->w, net->h);
+					//image sized = resize_image(im, net->w, net->h);
+					//image sized2 = resize_max(im, net->w);
+					//image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
+					//resize_network(net, sized.w, sized.h);
+					layer l = net->layers[net->n-1];
+
+					box *boxes = calloc(l.w*l.h*l.n, sizeof(box));
+					float **probs = calloc(l.w*l.h*l.n, sizeof(float *));
+					for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = calloc(l.classes + 1, sizeof(float *));
+					float **masks = 0;
+					if (l.coords > 4){
+						masks = calloc(l.w*l.h*l.n, sizeof(float*));
+						for(j = 0; j < l.w*l.h*l.n; ++j) masks[j] = calloc(l.coords-4, sizeof(float *));
+					}
+
+					float *X = sized.data;
+					time=what_time_is_it_now();
+					network_predict(net, X);
+					printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
+					get_region_boxes(l, im.w, im.h, net->w, net->h, thresh, probs, boxes, masks, 0, 0, hier_thresh, 1);
+					//if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+					if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+					draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, masks, names, alphabet, l.classes);
+					if(outfile){
+						save_image(im, outfile);
+					} else{
+						save_image(im, "predictions");
+#ifdef OPENCV
+						cvNamedWindow("predictions", CV_WINDOW_NORMAL); 
+						if(fullscreen){
+							cvSetWindowProperty("predictions", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+						}
+						show_image(im, "predictions");
+						cvWaitKey(0);
+						cvDestroyAllWindows();
+#endif
+					}
+
+
+					free_image(im);
+					free_image(sized);
+					free(boxes);
+					free_ptrs((void **)probs, l.w*l.h*l.n);
+
+					//      if (filename) break;
+				}
+				break;
+		}
+		dir = readdir(d);
+	}
+
+}
+
 void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
 {
 	list *options = read_data_cfg(datacfg);
@@ -575,102 +678,19 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
 	network *net = load_network(cfgfile, weightfile, 0);
 	set_batch_network(net, 1);
 	srand(2222222);
-	double time;
-	char *input = malloc(sizeof(char) * 256);
-	int j;
-	float nms=.3;
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
-	// ============================== My code ==============================
 
-	// Use DIR to go through all files under the directory
-	DIR *d = opendir(filename);
-	if (!d) {
-		fprintf(stderr, "Failed to open directory\n");
-		return;
-	}
-	struct dirent *dir = readdir(d);
-	char *file;
-	while (dir) {
-		file = dir -> d_name;
+	double start, end;
+	int count = 0;
+	start = what_time_is_it_now();
+	
 
-		// check if the file ends with .jpg
-		if (strstr(file, ".jpg") != NULL) {
-
-			strcpy(input, filename);
-			strcat(input, "/\0");
-			strcat(input, file);
-
-			fprintf(stdout, "File curringly processing: %s\n", input);
-
-			/*
-			   if(filename){
-			   strncpy(input, filename, 256);
-			   } else {
-			   printf("Enter Image Path: ");
-			   fflush(stdout);
-			   input = fgets(input, 256, stdin);
-			   if(!input) return;
-			   strtok(input, "\n");
-			   }
-			 */
-			image im = load_image_color(input,0,0);
-			image sized = letterbox_image(im, net->w, net->h);
-			//image sized = resize_image(im, net->w, net->h);
-			//image sized2 = resize_max(im, net->w);
-			//image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
-			//resize_network(net, sized.w, sized.h);
-			layer l = net->layers[net->n-1];
-
-			box *boxes = calloc(l.w*l.h*l.n, sizeof(box));
-			float **probs = calloc(l.w*l.h*l.n, sizeof(float *));
-			for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = calloc(l.classes + 1, sizeof(float *));
-			float **masks = 0;
-			if (l.coords > 4){
-				masks = calloc(l.w*l.h*l.n, sizeof(float*));
-				for(j = 0; j < l.w*l.h*l.n; ++j) masks[j] = calloc(l.coords-4, sizeof(float *));
-			}
-
-			float *X = sized.data;
-			time=what_time_is_it_now();
-			network_predict(net, X);
-			printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
-			get_region_boxes(l, im.w, im.h, net->w, net->h, thresh, probs, boxes, masks, 0, 0, hier_thresh, 1);
-			//if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-			if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-			draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, masks, names, alphabet, l.classes);
-			if(outfile){
-				save_image(im, outfile);
-			} else{
-				save_image(im, "predictions");
-#ifdef OPENCV
-				cvNamedWindow("predictions", CV_WINDOW_NORMAL); 
-				if(fullscreen){
-					cvSetWindowProperty("predictions", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
-				}
-				show_image(im, "predictions");
-				cvWaitKey(0);
-				cvDestroyAllWindows();
-#endif
-			}
+	_my_detect(names, net, filename, outfile, fullscreen, thresh, hier_thresh, alphabet, &count);
 
 
-			free_image(im);
-			free_image(sized);
-			free(boxes);
-			free_ptrs((void **)probs, l.w*l.h*l.n);
-#ifdef SINGLE
-			break;
-#endif
-		}
-		dir = readdir(d);
-	}
+	end = what_time_is_it_now();
+	fprintf(stdout, "Processing time: %lf\n", end - start);
+	fprintf(stdout, "FPS: %lf\n", count / (end - start));
+
 }
 
 void run_detector(int argc, char **argv)
